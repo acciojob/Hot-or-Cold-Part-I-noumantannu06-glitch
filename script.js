96d1ad7071ebc4e8670bcad871df88cb02af3aab
Md Nouman <noumantannu06@gmail.com>
2026-05-30 17:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
- const guessInput = document.getElementById("guess");
-    const checkBtn = document.getElementById("check");
-    const response = document.getElementById("response");
+const guessInput = document.getElementById("guess");
+  const checkBtn = document.getElementById("check");
+  const respond = document.getElementById("respond");
 
-    const secretNumber = Math.floor(Math.random() * 100) + 1;
-    let firstGuess = null;
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+  let firstGuess = null;
 
-    response.textContent = "";
+  respond.textContent = "";
 
-    checkBtn.addEventListener("click", () => {
-      const guess = Number(guessInput.value);
-      if (!guessInput.value) return;
+  checkBtn.addEventListener("click", () => {
+    const guess = Number(guessInput.value);
+    if (!guessInput.value) return;
 
-      if (firstGuess === null) {
-        firstGuess = guess;
-
-        if (guess < secretNumber) {
-          response.textContent = "guess higher";
-        } else if (guess > secretNumber) {
-          response.textContent = "guess lower";
-        } else {
-          response.textContent = "Correct!";
-        }
-        return;
-      }
-
-      const prevDiff = Math.abs(secretNumber - firstGuess);
-      const currentDiff = Math.abs(secretNumber - guess);
-
-      if (currentDiff < prevDiff) {
-        response.textContent =
-          guess < secretNumber
-            ? "Getting hotter, guess higher"
-            : "Getting hotter, guess lower";
-      } else if (guess < secretNumber) {
-        response.textContent = "guess higher";
-      } else if (guess > secretNumber) {
-        response.textContent = "guess lower";
-      } else {
-        response.textContent = "Correct!";
-      }
-
+    if (firstGuess === null) {
       firstGuess = guess;
-    });
+
+      if (guess < secretNumber) {
+        respond.textContent = "guess higher";
+      } else if (guess > secretNumber) {
+        respond.textContent = "guess lower";
+      } else {
+        respond.textContent = "Correct!";
+      }
+      return;
+    }
+
+    const prevDiff = Math.abs(secretNumber - firstGuess);
+    const currentDiff = Math.abs(secretNumber - guess);
+
+    if (currentDiff < prevDiff) {
+      respond.textContent =
+        guess < secretNumber
+          ? "Getting hotter, guess higher"
+          : "Getting hotter, guess lower";
+    } else if (guess < secretNumber) {
+      respond.textContent = "guess higher";
+    } else if (guess > secretNumber) {
+      respond.textContent = "guess lower";
+    } else {
+      respond.textContent = "Correct!";
+    }
+
+    firstGuess = guess;
+  });
